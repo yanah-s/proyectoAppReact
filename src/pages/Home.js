0631b826f0999e3c9c712a53d6dd3f61b6c0b22a
yana <yanahinasuarez@gmail.com>
2024-05-19 21:10:45 -0300

    master con pagina home

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bienvenido a la página principal</h1>
+        </div>
+    );
+};
+
+export default Home;
