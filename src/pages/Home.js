79d0edc9a5eea19ff4fd6ee394da3e98be1a3e6e
yana <yanahinasuarez@gmail.com>
2024-05-25 19:31:40 -0300

    incorporacion de MUI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Button } from '@mui/material';
+
 
 const Home = () => {
+    const agendar = () => {
+        // Lógica para manejar el click del botón
+        console.log('¡Agéndate!');
+    };
+
     return (
         <div>
-            <h1>Bienvenido a la página principal</h1>
+            <h1>Bienvenido!</h1>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={agendar}
+            >
+                Agéndate!
+            </Button>
+            <h3> texto de la cliente</h3>
         </div>
     );
 };
