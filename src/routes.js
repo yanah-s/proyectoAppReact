84478fd4b3e84d03c19b49875d0f43dc9cfd4211
yana <yanahinasuarez@gmail.com>
2024-06-10 21:09:20 -0300

    front de disponibilidad de agenda

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import DisponibilidadAgenda from './pages/DisponibilidadAgenda';
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/DisponibilidadAgenda" element={<DisponibilidadAgenda />} />
             </Routes>
         </Router>
     );
